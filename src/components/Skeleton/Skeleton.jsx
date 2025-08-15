edb0994a024ebf63b6a5446550f20570cdfaf784
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-08-15 21:47:47 +0300

    2

---
+++ src/components/Skeleton/Skeleton.jsx
@@ -0,0 +1,22 @@
+import styles from './styles.module.css';
+
+const Skeleton = ({ count = 1, type = 'banner' }) => {
+   return (
+      <>
+         {count > 1 ? (
+            <ul>
+               {[Array(count)].map((_, index) => (
+                  <li
+                     key={index}
+                     className={type === 'banner' ? styles.banner : styles.item}
+                  ></li>
+               ))}
+            </ul>
+         ) : (
+            <div className={type === 'banner' ? styles.banner : styles.item}></div>
+         )}
+      </>
+   );
+};
+
+export default Skeleton;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,9 +4,11 @@ import { getNews } from '../../api/apiNews';
 import NewsBanner from '../../components/NewsBanner/NewsBanner';
 import mockNews from '../../data/mockNews';
 import NewsList from '../../components/NewsList/NewsList';
+import Skeleton from '../../components/Skeleton/Skeleton';
 
 const Main = () => {
    const [news, setNews] = useState([]);
+   const [isLoading, setIsLoading] = useState(true);
 
    useEffect(() => {
       const fetchNews = async () => {
@@ -21,6 +23,8 @@ const Main = () => {
             }
          } catch (error) {
             console.log(error);
+         } finally {
+            // setIsLoading(false);
          }
       };
 
@@ -29,7 +33,12 @@ const Main = () => {
 
    return (
       <main className={styles.main}>
-         {news.length > 0 ? <NewsBanner item={news[0]} /> : null}
+         {news.length > 0 && !isLoading ? (
+            <NewsBanner item={news[0]} />
+         ) : (
+            <Skeleton type="banner" count={1} />
+         )}
+
          <NewsList news={news} />
       </main>
    );
